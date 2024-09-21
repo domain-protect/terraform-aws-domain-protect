5e1f5bad94a4a4e04e68bb98346748cb4cdf1952
nitrocode <7775707+nitrocode@users.noreply.github.com>
2024-09-21 14:54:36 -0500

    chore: use terraform to read local .python-version file
    Signed-off-by: nitrocode <7775707+nitrocode@users.noreply.github.com>

--- locals.tf
+++ locals.tf
@@ -1,4 +1,6 @@
 locals {
   env                    = coalesce(var.environment, lower(terraform.workspace))
   production_environment = coalesce(var.production_environment, var.production_workspace)
+
+  runtime = coalesce(var.runtime, format("python%s", regex("^[0-9]+\\.[0-9]+", file("${path.module}/.python-version"))))
 }
