40f5ee5e1e095f3997583c2720329652b3795d71
Paul Schwarzenberger <paul@celidor.net>
2024-10-03 21:35:41 +0100

    tests: remove Terraform workspace refs

--- examples/dev/main.tf
+++ examples/dev/main.tf
@@ -1,25 +1,21 @@
 module "domain_protect" {
-  #source = "../../"
-  source  = "domain-protect/domain-protect/aws"
-  version = "0.5.1"
+  source = "../../"
+  # source  = "domain-protect/domain-protect/aws"
+  # version = "0.5.1"
 
   allowed_regions     = "['eu-west-1', 'eu-west-2', 'us-east-1']"
   cf_api_key          = var.cf_api_key
   cloudflare          = true
+  environment         = "dev"
   external_id         = var.external_id
   hackerone           = "enabled"
   ip_address          = true
-  ip_scan_schedule    = "10 minutes"
-  ip_time_limit       = "0.1" #6 minutes
+  ip_time_limit       = 0.1 # 6 minutes 
   org_primary_account = var.org_primary_account
   rcu                 = 1
-  slack_channels      = terraform.workspace == "prd" ? ["devsecops"] : ["devsecops-dev"]
-  scan_schedule       = "10 minutes"
+  slack_channels      = ["devsecops-dev"]
   slack_webhook_type  = "app"
   slack_webhook_urls  = var.slack_webhook_urls
-  takeover            = terraform.workspace == "prd" ? true : false
-  update_schedule     = "10 minutes"
+  takeover            = false
   wcu                 = 1
-
-
 }
