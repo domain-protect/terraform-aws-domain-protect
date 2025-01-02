03c2f5bd7005d9ef78d4635227a593648eed61b9
RB <7775707+nitrocode@users.noreply.github.com>
2025-01-02 14:35:18 -0600

    fix: add data source for managed policies

--- modules/iam/data.tf
+++ modules/iam/data.tf
@@ -1,1 +1,11 @@
 data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy" "default" {
+  for_each = toset([
+    "AdministratorAccess-AWSElasticBeanstalk",
+    "AWSCloudFormationFullAccess",
+    "AmazonS3FullAccess",
+    "AmazonVPCFullAccess", 
+  ])
+  name = each.value
+}
