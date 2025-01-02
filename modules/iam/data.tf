50bfc02805d4b4c141b91566736e865ae56fcb69
RB <7775707+nitrocode@users.noreply.github.com>
2025-01-02 14:40:33 -0600

    fix: use var

--- modules/iam/data.tf
+++ modules/iam/data.tf
@@ -1,11 +1,6 @@
 data "aws_caller_identity" "current" {}
 
 data "aws_iam_policy" "default" {
-  for_each = var.takeover ? toset([
-    "AdministratorAccess-AWSElasticBeanstalk",
-    "AWSCloudFormationFullAccess",
-    "AmazonS3FullAccess",
-    "AmazonVPCFullAccess",
-  ]) : toset([])
-  name = each.value
+  for_each = var.takeover ? toset(var.managed_policy_names) : toset([])
+  name     = each.value
 }
