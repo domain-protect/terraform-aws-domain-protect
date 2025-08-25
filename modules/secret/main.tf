b25c22b5e5bb3ebe4ca0806f0116c96a20f54337
Paul Schwarzenberger <paul@celidor.net>
2025-08-25 17:32:41 +0100

    Remove Checkov pre-commit due to ARM incompatibility

--- modules/secret/main.tf
+++ modules/secret/main.tf
@@ -1,11 +1,5 @@
-resource "random_string" "value" {
-  length  = 5
-  special = false
-  upper   = false
-}
-
 resource "aws_secretsmanager_secret" "secret" {
-  name        = "${lower(var.project)}-${lower(var.purpose)}-${lower(var.environment)}-${random_string.value.result}"
+  name        = "${lower(var.project)}-${lower(var.purpose)}-${lower(var.environment)}"
   description = var.description
   kms_key_id  = var.kms_key_id == "" ? null : var.kms_key_id
   tags        = var.tags
